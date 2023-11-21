368d1ecaf42ecd62a09db09d01d5374efd80891d
rein <reinX244@gmail.com>
2023-11-21 22:24:53 +0800

    Added CompoundInterestCalculator, ForLoops, and WhileLoops.

---
+++ Bro Code Python Tutorials/ForLoops/main.py
@@ -0,0 +1,30 @@
+# for loops = execute a block of code a fixed number of time.
+#             You can iterate over a range, string, sequence, etc.
+
+# Print numbers from 1 to 10
+# for x in range(1, 11):
+#     print(x)
+
+# Print the numbers from 10 to 1
+# for x in reversed(range(1, 11)):
+#     print(x)
+
+# Count by 2's
+# for x in range(1, 11, 2):
+#     print(x)
+# print('HAPPY NEW YEAR!')
+
+# Iterating through a string
+# credit_card = '1234-5678-9012-3456'
+# for i in credit_card:
+#     print(i)
+
+# Count from 1 to 20
+# for x in range(1, 21):
+#     if x == 13:
+        # skips over the current loop
+        # continue
+        # terminates the loop
+        # break
+    # else:
+    #     print(x)
---
+++ Bro Code Python Tutorials/WhileLoops/main.py
@@ -0,0 +1,25 @@
+# while loop = execute some code WHILE some condition remains true
+# name = input('Enter your name: ')
+
+# while name == "":
+#     print('You did not enter your name!')
+#     name = input('Enter your name: ')
+# print(f'Hello {name}')
+
+# age = int(input('Enter your age: '))
+# while age < 0:
+#     print('Age can\'t be negative!')
+#     age = int(input('Enter your age: '))
+# print(f'You are {age} years old.')
+
+# food = input('Enter a food you like (q to quit): ')
+# while not food == 'q':
+#     print(f'You like {food}')
+#     food = input('Enter another food you like (q to quit): ')
+# print('bye')
+
+# num = int(input('Enter a # between 1 - 10: '))
+# while num < 1 or num > 10:
+#     print(f'{num} is not valid!')
+#     num = int(input('Enter a # between 1 - 10: '))
+# print(f'Your number is {num}')
