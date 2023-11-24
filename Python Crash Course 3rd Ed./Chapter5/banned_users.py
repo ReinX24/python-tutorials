b0db8e5bae8f7f0a910557aa1cedcaa62272df45
rein <reinX244@gmail.com>
2023-11-24 22:45:53 +0800

    Added Chapter5 project folder.

---
+++ Python Crash Course 3rd Ed./Chapter5/banned_users.py
@@ -0,0 +1,6 @@
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+# Checks if user is not in the banned_users list
+if user not in banned_users:
+	print(f'{user.title()}, you can post a response if you wish.')
---
+++ Python Crash Course 3rd Ed./Chapter5/conditional_tests.py
@@ -0,0 +1,58 @@
+# 5 - 1 Conditional Tests
+# car = 'subaru'
+# print('Is car == \'subaru\'? I predict True.')
+# print(car == 'subaru')
+
+# print('\nIs car == \'audi\'? I predict False.')
+# print(car == 'audi')
+
+# Test 1 & 2
+# my_name = 'rein'
+# print(my_name == 'rein')
+# print(my_name == 'rain')
+
+# Test 3 & 4
+# my_phone = 'android'
+# print(my_phone == 'ios')
+# print(my_phone == 'android')
+
+# Test 5 & 6
+# main_champion = 'zed'
+# print(main_champion == 'zed')
+# print(main_champion == 'yasuo')
+
+# Test 7 & 8
+# processor = '5600G'
+# print(processor == '5700G')
+# print(processor == '5600G')
+
+# Test 9 & 10
+# my_hobby = 'programming'
+# print('Does my_hobby == \'programming\'? I predict True.')
+# print(my_hobby == 'programming')
+#
+# print('\nDoes my_hobby == \'smoking\'? I predict False.')
+# print(my_hobby == 'smoking')
+
+# 5 - 2 More Conditional Tests
+# Equality and inequality with strings
+# Tests using the lower() method
+# my_name = 'Rein'
+
+# if my_name.lower() == 'rein':
+# 	print(f'Welcome back {my_name}!')
+# else:
+# 	print('Who are you?')
+
+# Numerical tests involving equality and inequality, greater than
+# and less than, greater than or equal to, and less than or equal to
+user_age = int(input('Enter your age: '))
+
+# if user_age > 18:
+# 	print('You are above 18!')
+# if user_age < 18:
+# 	print('You are below 18!')
+# if user_age >= 18:
+# 	print('You are 18 or above!')
+if user_age <= 18:
+	print('You are 18 or below!')
