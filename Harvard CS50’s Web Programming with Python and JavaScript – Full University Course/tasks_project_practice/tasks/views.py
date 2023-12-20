bda682a6d0694ef86dca46569a566b78010ccb55
rein <reinX244@gmail.com>
2023-12-20 21:00:07 +0800

    Finished tasks_project_practice by finishing functionalities and adding bootstrap css.

--- Harvard CS50’s Web Programming with Python and JavaScript – Full University Course/tasks_project_practice/tasks/views.py
+++ Harvard CS50’s Web Programming with Python and JavaScript – Full University Course/tasks_project_practice/tasks/views.py
@@ -1,14 +1,42 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    """Class for storing form components."""
+    # Creating a text field with a label and classes.
+    task_field = forms.CharField(label="New Task", 
+                                 widget=forms.TextInput(
+                                     attrs={'class': 'form-control', 
+                                            'placeholder': 'Enter task...'}))
 
 def index(request):
     """Index of our tasks app."""
+    # Checks if there are tasks already stored, if not create a new list.
     if "tasks" not in request.session:
         request.session["tasks"] = []
-
+        
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"],
     })
 
 def add(request):
     """Adding stored tasks to our app."""
-    return render(request, "tasks/add.html")
+    # Checks if the request made is POST, from the form.
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        # Checks if the form is valid.
+        if form.is_valid():
+            new_task = form.cleaned_data["task_field"]
+            request.session["tasks"] += [new_task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            # Returns the same page with an error message.
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
