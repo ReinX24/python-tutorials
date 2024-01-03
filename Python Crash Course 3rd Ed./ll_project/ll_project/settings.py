0fcd49d68289609ceb1bb1709c99566922d44493
rein <reinX244@gmail.com>
2024-01-03 21:00:15 +0800

    Finished styling blogs app in Blog project and started learning about deploying to platform.sh.

--- Python Crash Course 3rd Ed./ll_project/ll_project/settings.py
+++ Python Crash Course 3rd Ed./ll_project/ll_project/settings.py
@@ -131,3 +131,28 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Platform.sh settings.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append(".platformsh.site")
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / "static"
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials("database")
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": db_settings["path"],
+                "USER": db_settings["username"],
+                "PASSWORD": db_settings["password"],
+                "HOST": db_settings["host"],
+                "PORT": db_settings["port"],
+            },
+        }
