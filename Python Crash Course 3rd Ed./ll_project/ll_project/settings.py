ea004b39ba56138b39aa9c78eabb84b0b8aad721
rein <reinX244@gmail.com>
2024-01-06 23:55:54 +0800

    Removed requirements text files.

--- Python Crash Course 3rd Ed./ll_project/ll_project/settings.py
+++ Python Crash Course 3rd Ed./ll_project/ll_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-mar1_4vq--f+78re)0p$vp1i_o(cak07+*9+$gcu&dg=zf0!)3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".pythonanywhere.com"]
 
 # My settings
 # Redirecting the user to learning_logs index when they log in.
@@ -133,26 +134,26 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Platform.sh settings.
-from platformshconfig import Config
+# from platformshconfig import Config
 
-config = Config()
-if config.is_valid_platform():
-    ALLOWED_HOSTS.append(".platformsh.site")
+# config = Config()
+# if config.is_valid_platform():
+#     ALLOWED_HOSTS.append(".platformsh.site")
 
-    if config.appDir:
-        STATIC_ROOT = Path(config.appDir) / "static"
-    if config.projectEntropy:
-        SECRET_KEY = config.projectEntropy
+#     if config.appDir:
+#         STATIC_ROOT = Path(config.appDir) / "static"
+#     if config.projectEntropy:
+#         SECRET_KEY = config.projectEntropy
 
-    if not config.in_build():
-        db_settings = config.credentials("database")
-        DATABASES = {
-            "default": {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": db_settings["path"],
-                "USER": db_settings["username"],
-                "PASSWORD": db_settings["password"],
-                "HOST": db_settings["host"],
-                "PORT": db_settings["port"],
-            },
-        }
+#     if not config.in_build():
+#         db_settings = config.credentials("database")
+#         DATABASES = {
+#             "default": {
+#                 "ENGINE": "django.db.backends.postgresql",
+#                 "NAME": db_settings["path"],
+#                 "USER": db_settings["username"],
+#                 "PASSWORD": db_settings["password"],
+#                 "HOST": db_settings["host"],
+#                 "PORT": db_settings["port"],
+#             },
+#         }
