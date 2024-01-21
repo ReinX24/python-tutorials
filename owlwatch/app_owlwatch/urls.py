276bf6c79e5c0771ab7eb643798b1f0b3a959231
rein <reinX244@gmail.com>
2024-01-21 22:54:30 +0800

    Added the ability to reset the answers in the test_page.

--- owlwatch/app_owlwatch/urls.py
+++ owlwatch/app_owlwatch/urls.py
@@ -6,6 +6,7 @@ app_name = "app_owlwatch"
 urlpatterns = [
     path("index/", views.index, name="index"),
     path("apps/", views.apps_page, name="apps_page"),
-    path("test/<int:user_id>", views.test_page, name="test_page"),
+    path("test/<int:user_id>/", views.test_page, name="test_page"),
+    path("test_reset/<int:user_id>/", views.test_reset, name="test_reset"),
     # path("test_history/<int:user_id>", views.test_history, name="test_history_page"),
 ]
--- owlwatch/app_owlwatch/views.py
+++ owlwatch/app_owlwatch/views.py
@@ -53,8 +53,68 @@ def test_page(request, user_id):
             context = {
                 "average_intensity": float(user_averages.average_intensity),
                 "average_frequency": float(user_averages.average_frequency),
+                "user_id": user_id,
             }
 
             return render(request, "app_owlwatch/results.html", context)
+
+    return render(request, "app_owlwatch/test.html", {"form": form, "user_id": user_id})
+
+
+@login_required
+def test_reset(request, user_id):
+    """Reset the values in our test_page."""
+    questions_record = Questions.objects.get(owner_id=user_id)
+
+    if request.method != "POST":
+        # Reset all the values of test_reset
+        questions_record.question1_intensity = 0
+        questions_record.question2_intensity = 0
+        questions_record.question3_intensity = 0
+        questions_record.question4_intensity = 0
+        questions_record.question5_intensity = 0
+        questions_record.question6_intensity = 0
+        questions_record.question7_intensity = 0
+        questions_record.question8_intensity = 0
+        questions_record.question9_intensity = 0
+        questions_record.question10_intensity = 0
+        questions_record.question11_intensity = 0
+        questions_record.question12_intensity = 0
+        questions_record.question13_intensity = 0
+        questions_record.question14_intensity = 0
+        questions_record.question15_intensity = 0
+        questions_record.question16_intensity = 0
+        questions_record.question17_intensity = 0
+
+        questions_record.question1_frequency = 0
+        questions_record.question2_frequency = 0
+        questions_record.question3_frequency = 0
+        questions_record.question4_frequency = 0
+        questions_record.question5_frequency = 0
+        questions_record.question6_frequency = 0
+        questions_record.question7_frequency = 0
+        questions_record.question8_frequency = 0
+        questions_record.question9_frequency = 0
+        questions_record.question10_frequency = 0
+        questions_record.question11_frequency = 0
+        questions_record.question12_frequency = 0
+        questions_record.question13_frequency = 0
+        questions_record.question14_frequency = 0
+        questions_record.question15_frequency = 0
+        questions_record.question16_frequency = 0
+        questions_record.question17_frequency = 0
+
+        form = QuestionsForm(instance=questions_record)
+
+        user_averages = Averages.objects.get(owner_id=user_id)
+        user_averages.average_intensity = 0
+        user_averages.average_frequency = 0
+
+        if form.is_valid():
+            user_record = form.save(commit=False)
+            user_record.owner = request.user
+            user_averages.owner = request.user
+            user_record.save()
+            user_averages.save()
 
     return render(request, "app_owlwatch/test.html", {"form": form, "user_id": user_id})
