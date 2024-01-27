a7b351db5993c8bd391a7ef341470e944f35fcd3
rein <reinX244@gmail.com>
2024-01-27 17:51:45 +0800

    Finished the functionality of editing and deleting Entry objects.

--- owlwatch/journal/urls.py
+++ owlwatch/journal/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("add_entry/<int:user_id>/", views.add_entry, name="add_entry"),
     path("entry_info/<int:entry_id>/", views.entry_info, name="entry_info"),
     path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
+    path("delete_entry/<int:entry_id>/", views.delete_entry, name="delete_entry"),
 ]
--- owlwatch/journal/views.py
+++ owlwatch/journal/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from journal.forms import EntryForm
-
 from journal.models import Entry
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def entries(request, user_id):
     """Show the user their entries."""
     entries = Entry.objects.filter(owner=user_id).order_by("created_date")
@@ -12,6 +13,7 @@ def entries(request, user_id):
     return render(request, "journal/entries.html", context)
 
 
+@login_required
 def add_entry(request, user_id):
     """Add an entry for our user."""
     if request.method == "POST":
@@ -27,6 +29,7 @@ def add_entry(request, user_id):
     return render(request, "journal/add_entry.html", {"form": form, "user_id": user_id})
 
 
+@login_required
 def entry_info(request, entry_id):
     """Show the full text of the Entry."""
     entry = Entry.objects.get(id=entry_id)
@@ -36,6 +39,30 @@ def entry_info(request, entry_id):
     )
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing Entry of the user."""
-    return render(request, "journal/edit_entry.html")
+    entry = Entry.objects.get(id=entry_id)
+
+    if request.method == "POST":
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.owner = request.user
+            entry.save()
+            return redirect("journal:entry_info", entry_id=entry_id)
+    else:
+        form = EntryForm(instance=entry)
+
+    return render(request, "journal/edit_entry.html", {"form": form, "entry": entry})
+
+
+@login_required
+def delete_entry(request, entry_id):
+    """Delete an existing Entry of the user."""
+    entry = Entry.objects.get(id=entry_id)
+    if request.method == "POST":
+        entry.delete()
+        return redirect("journal:entries", user_id=request.user.id)
+
+    return render(request, "journal/delete_entry.html", {"entry": entry})
